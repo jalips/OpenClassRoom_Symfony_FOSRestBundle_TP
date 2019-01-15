9488c7a13ed81ee922b23eb381d8eb9504fc09d1
jalips <jalips@protonmail.com>
2019-01-15 15:56:04 +0100

    finish tp

--- src/AppBundle/Controller/ArticleController.php
+++ src/AppBundle/Controller/ArticleController.php
@@ -9,6 +9,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 use FOS\RestBundle\Request\ParamFetcherInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\FOSRestController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Validator\ConstraintViolationList;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -90,4 +91,65 @@ class ArticleController extends FOSRestController
 
         return $article;
     }
+
+    /**
+     * @Rest\Delete(
+     *     path = "/articles/{id}",
+     *     name = "app_article_delete",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 200)
+     */
+    public function deleteAction(Article $article, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+
+            throw new ResourceValidationException($message);
+        }
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($article);
+        $em->flush();
+
+        return $article;
+    }
+
+    /**
+     * @Rest\Put(
+     *     path = "/articles/{id}",
+     *     name = "app_article_update",
+     *     requirements = {"id"="\d+"}
+     * )
+     * @Rest\View(StatusCode = 200)
+     * @ParamConverter("article", converter="fos_rest.request_body")
+     */
+    public function updateAction(Article $articleToUpdate, Article $article, ConstraintViolationList $violations)
+    {
+        if (count($violations)) {
+            $message = 'The JSON sent contains invalid data. Here are the errors you need to correct: ';
+            foreach ($violations as $violation) {
+                $message .= sprintf("Field %s: %s ", $violation->getPropertyPath(), $violation->getMessage());
+            }
+
+            throw new ResourceValidationException($message);
+        }
+
+        //var_dump($articleToUpdate); var_dump($article); die();
+
+        $articleToUpdate->setContent($article->getContent());
+        $articleToUpdate->setTitle($article->getTitle());
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($articleToUpdate);
+        $em->flush();
+
+        return $article;
+    }
+
 }
